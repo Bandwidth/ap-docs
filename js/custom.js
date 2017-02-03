f9bf0532a55c0a16660fcf17553321e620fc3650
Bernard Brown <bbrown@bandwidth.com>
2017-02-03 15:21:58 -0500

    Adjusting gap above lang-switcher

--- js/custom.js
+++ js/custom.js
@@ -26,6 +26,7 @@ module.exports = function ($) {
 	}
 	$('li.chapter a:contains(FAQ)').append('<i class="icons8-open-in-window" style="float:right;"></i>');
 	$('li.chapter a:contains(FAQ)').css('width','100%');
+	$('.markdown-section').has('.api-method-code').css('padding-top','0px');
 
 	return $.html();
 }
