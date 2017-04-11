4087b348e24e0bf68730d1d89bf8a350c3511b7e
Daniel Tolbert <danielptolbert@gmail.com>
2017-04-11 18:13:14 -0400

    add files and update travs yaml (#24)
    * add files and update travis yaml to build and deploy and comment on pull requests

---
+++ comment.py
@@ -0,0 +1,50 @@
+
+#!/usr/bin/env python
+"""
+Comments stdin to the GitHub PR that triggered the travis build.
+Usage:
+    flake8 | python comment-on-pr.py
+Notes:
+    The following enviromental variables need to be set:
+    - TRAVIS_PULL_REQUEST
+    - TRAVIS_REPO_SLUG
+    - TRAVIS_BOT_GITHUB_TOKEN
+"""
+import os
+import sys
+import json
+import requests
+# import urllib3
+# urllib3.disable_warnings()
+
+GITHUB_API_URL = 'https://api.github.com'
+
+
+def comment_on_pull_request(pr_number, slug, token, comment):
+    """ Comment message on a given GitHub pull request. """
+    url = '{api_url}/repos/{slug}/issues/{number}/comments'.format(
+        api_url=GITHUB_API_URL, slug=slug, number=pr_number)
+    print(url)
+    response = requests.post(url, data=json.dumps({'body': comment}),
+                             headers={'Authorization': 'token ' + token})
+    print(response)
+    return response.json()
+
+
+if __name__ == '__main__':
+    PR_NUMBER = os.environ.get('TRAVIS_PULL_REQUEST')
+    REPO_SLUG = os.environ.get('TRAVIS_REPO_SLUG')
+    TOKEN = os.environ.get('TRAVIS_BOT_GITHUB_TOKEN')
+    branch_name = os.environ.get('TRAVIS_PULL_REQUEST_BRANCH')
+    if PR_NUMBER == "false":
+        sys.exit(0)
+    site_name = 'bw-' + branch_name
+
+    comment = "Preview Changes at:\nhttp://%s.s3-website-us-east-1.amazonaws.com/" % site_name
+
+    if all([PR_NUMBER, REPO_SLUG, TOKEN]):
+        print('Commenting on Pull Request')
+        res = comment_on_pull_request(PR_NUMBER, REPO_SLUG, TOKEN, comment)
+        print(res)
+    else:
+        print('Not all neccesery variables are present')
---
+++ make_yaml.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+def main():
+    branch_name = os.environ.get('TRAVIS_PULL_REQUEST_BRANCH')
+    PR_NUMBER = os.environ.get('TRAVIS_PULL_REQUEST')
+    if PR_NUMBER == False:
+        sys.exit(0)
+    site_name = 'bw-' + branch_name
+    yaml_contents = "site: _book\ns3_bucket: " + site_name
+    with open('.s3_website.yaml', 'w') as yaml_file:
+        yaml_file.write(yaml_contents)
+
+
+# Call the main() function to begin the program.
+if __name__ == '__main__':
+    main()
